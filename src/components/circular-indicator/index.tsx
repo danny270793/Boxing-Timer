af1b5d5554ef4845782c8d7d4ffc44fd1d9846d2
Danny Vaca <dvaca@technisys.com>
2025-06-27 17:03:39 -0500

    allow to have external children

--- src/components/circular-indicator/index.tsx
+++ src/components/circular-indicator/index.tsx
@@ -3,15 +3,15 @@ import './index.css'
 
 interface CircularIndicatorProps {
   percentage: number
+  children?: ReactNode
 }
 
 export const CircularIndicator: FC<CircularIndicatorProps> = (
   props: CircularIndicatorProps,
 ): ReactNode => {
   return (
-    <div
-      class="circular-progress"
-      style={{ '--percentage': props.percentage }}
-    ></div>
+    <div class="circular-progress" style={{ '--percentage': props.percentage }}>
+      <div className="circular-progress-children">{props.children}</div>
+    </div>
   )
 }
