a6956f7d8a7adadbe18be5b4a6c9c8dbb1e23633
Danny Vaca <dvaca@technisys.com>
2025-06-27 17:29:36 -0500

    validate arguments

--- src/components/circular-indicator/index.tsx
+++ src/components/circular-indicator/index.tsx
@@ -4,14 +4,29 @@ import './index.css'
 interface CircularIndicatorProps {
   percentage: number
   children?: ReactNode
+  size?: number
+  innerSize?: number
 }
 
 export const CircularIndicator: FC<CircularIndicatorProps> = (
   props: CircularIndicatorProps,
 ): ReactNode => {
   return (
-    <div class="circular-progress" style={{ '--percentage': props.percentage }}>
-      <div className="circular-progress-children">{props.children}</div>
+    <div
+      class="circular-progress"
+      style={{
+        '--percentage': props.percentage,
+        width: props.size ? `${props.size}px` : '100vw',
+      }}
+    >
+      <div
+        className="circular-progress-children"
+        style={{
+          width: props.innerSize ? `${props.innerSize * 0.9}px` : '80vw',
+        }}
+      >
+        {props.children}
+      </div>
     </div>
   )
 }
