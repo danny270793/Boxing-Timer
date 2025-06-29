05da9b3d16a970893ce8dd11da298e1e25cfff3c
Danny Vaca <dvaca@technisys.com>
2025-06-29 13:13:02 -0500

    capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'io.github.danny270793.boxingtimer',
+  appName: 'Boxing Timer',
+  webDir: 'dist',
+}
+
+export default config
