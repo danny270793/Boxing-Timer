90e700b21d2a86b19a18b54e027857c4c8f8d5b0
Danny Vaca <danny270793@icloud.com>
2026-09-05 19:30:58 -0500

    chore: remove capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli'
-
-const config: CapacitorConfig = {
-  appId: 'io.github.danny270793.boxingtimer',
-  appName: 'Boxing Timer',
-  webDir: 'dist',
-}
-
-export default config
